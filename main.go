7c14c84e8d128d88f2eb6b11f5234b8471701042
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-03-10 15:08:10 +0000

    Add call to use kubeconfig and get namespaces in cluster

--- main.go
+++ main.go
@@ -9,4 +9,11 @@ import (
 func main() {
 	//get the k8s client to access the Cloud Platform
 	client := client.GetKubernetesClient()
+	ns, nsError := client.CoreV1().Namespaces().List(metav1.ListOptions{})
+	if nsError != nil {
+		log.Fatalf("Can't list namespaces ", nsError)
+	}
+	for i := range ns.Items {
+		log.Info("Namespace/project : ", ns.Items[i].Name)
+	}
 }
